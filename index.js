df7d746c86207fc39c8673dae4c99b5782085e9b
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-03-07 14:23:11 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Code your solutions in this file
+// for ([initialization]; [condition]; [iteration]) {
+//     [loop body]
+//   }
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for(let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+
+function writeCards(names, event) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+writeCards(["john", "Joe", "Mike"])
+
+// while ([condition]) {
+//     [loop body]
+//   }
+
+// const gifts = ["teddy bear", "droe", "doll"];
+
+// function wrapGifts(gifts) {
+//     let i = 0;
+//     while (i < gifts.length) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         i++;
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
